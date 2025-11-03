e2045029f34fdad95e67f08dade76372b3d22bbb
JakirHossain25 <sarkerjakariya5@gmail.com>
2025-11-04 00:40:25 +0600

    adv qr code

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import qrcode as a
+import qrcode
+from PIL import Image
 
-img= a.make("this is a fitst qr code")
-img.save("qr.png")
+qr= qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=3)
+qr.add_data("hello thi is a update qr code")
+qr.make(True)
+Img=qr.make_image(fill_color="red",back_color="blue")
+Img.save("advence qr code.png")
